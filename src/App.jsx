74e8d7491b7deee15a451ae615a05e6b2426b341
ManewJ <manoeljustino@gmail.com>
2026-06-19 12:06:07 -0300

    chore: setup inicial do projeto com Vite, React e Tailwind

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="bg-black min-h-screen flex items-center justify-center">
+      <h1 className="text-orange-500 text-4xl font-bold">TrackRun.</h1>
+    </div>
+  )
+}
+
+export default App
